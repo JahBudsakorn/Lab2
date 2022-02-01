637a5ad04077045f1de74657eb7fc96b4f3fc426
Asus <Asus@LAPTOP-ENF3J4UI>
2022-02-02 00:07:11 +0700

    Lab2

---
+++ Lab2/src/lab2/lab.java
@@ -0,0 +1,21 @@
+package lab2;
+import java.util.Scanner ;
+public class lab {
+	static void showNum(int num) {
+		for(int i = 1 ; i <= num ; i++) {
+			System.out.print(i) ;
+		if(i%5 != 0 && i != num) {
+			System.out.print(", ") ;
+		} else {
+			System.out.println() ; }
+		}//end for
+	}//end show num
+
+public static void main(String[] args) {
+	Scanner userInput = new Scanner(System.in) ;
+	System.out.print("Please enter your value : ") ;
+	int num = userInput.nextInt() ;
+	System.out.println("-------------------") ;
+	showNum(num) ;
+ 	}//end main
+}//end class
